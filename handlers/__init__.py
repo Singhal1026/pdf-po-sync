131eeafd574fd10a9a7372946c1f1201a7a56acb
Yash Singhal <91470295+Singhal1026@users.noreply.github.com>
2026-04-04 15:23:09 +0530

    Add multiple handlers to the init file

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,1 +1,10 @@
+from handlers.croma import CromaHandler
+from handlers.reliance import RelianceHandler
+from handlers.zepto import ZeptoHandler
 
+
+HANDLERS = [
+    CromaHandler(),
+    RelianceHandler(),
+    ZeptoHandler()
+]
